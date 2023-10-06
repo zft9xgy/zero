358796beeb370d595905347cb0d2c8c9cc006ee4
zft9xgy <zft9xgy@icloud.com>
2023-10-06 12:37:46 +0200

    Update content

---
+++ scripts/commit-update-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Este script facilita el proceso de update y deploy con hugo.
+#
+#
+# Pseudo codigo
+# 1. Eliminar la carpeta public
+# 2. Hugo realiza el nuevo build
+# 3. Git add . & commmit & push
+#
+# se debe ejecutar desde el directoria base donde se encuentra hugo
+
+rm -rf ./public
+hugo
+git add .
+git commit -m "Update content"
+git push
